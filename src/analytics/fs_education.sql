8ead359a8632209fbac514e622649ae2f2dec0c0
mottamatheus835 <mottamatheus835@gmail.com>
2026-07-03 18:27:01 -0300

    atualizando código sql

--- src/analytics/fs_education.sql
+++ src/analytics/fs_education.sql
@@ -27,39 +27,40 @@ tb_percent_curso AS (
  LEFT JOIN tb_curso AS T2 ON T1.descSlugCurso = T2.descSlugCurso
  GROUP BY T1.idUsuario, T1.descSlugCurso)
 
- SELECT idUsuario, 
+ SELECT 
+    idUsuario, 
 
-
-SUM(CASE desSlugCurso = 'carreira' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'coleta-dados-2024' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'ds-databricks-2024' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'ds-pontos-2024' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'estatistica-2024' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'estatistica-2025' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'f1-lake' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'github-2024' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'github-2025' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'go-2026' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'ia-canal-2025' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'lago-mago-2024' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'loyalty-predict-2025' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'machine-learning-2025' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'matchmaking-trampar-de-casa-2024' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'ml-2024' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'mlflow-2025' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'nekt-2025' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'pandas-2024' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'pandas-2025' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'plataforma-ml-2026' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'python-2024' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'python-2025' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'ragia' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'speed-f1' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'sql-2020' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'sql-2025' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'streamlit-2025' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'trampar-lakehouse-2024' THEN percentCursoCompleto ELSE 0) AS 
-SUM(CASE desSlugCurso = 'tse-analytics-2024' THEN percentCursoCompleto ELSE 0) AS 
+    SUM(CASE WHEN descSlugCurso = 'carreira' THEN percentCursoCompleto ELSE 0 END) AS carreira,
+    SUM(CASE WHEN descSlugCurso = 'coleta-dados-2024' THEN percentCursoCompleto ELSE 0 END) AS coletaDados2024,
+    SUM(CASE WHEN descSlugCurso = 'ds-databricks-2024' THEN percentCursoCompleto ELSE 0 END) AS dsDatabricks2024,
+    SUM(CASE WHEN descSlugCurso = 'ds-pontos-2024' THEN percentCursoCompleto ELSE 0 END) AS dsPontos2024,
+    SUM(CASE WHEN descSlugCurso = 'estatistica-2024' THEN percentCursoCompleto ELSE 0 END) AS estatistica2024,
+    SUM(CASE WHEN descSlugCurso = 'estatistica-2025' THEN percentCursoCompleto ELSE 0 END) AS estatistica2025,
+    SUM(CASE WHEN descSlugCurso = 'f1-lake' THEN percentCursoCompleto ELSE 0 END) AS f1Lake,
+    SUM(CASE WHEN descSlugCurso = 'github-2024' THEN percentCursoCompleto ELSE 0 END) AS github2024,
+    SUM(CASE WHEN descSlugCurso = 'github-2025' THEN percentCursoCompleto ELSE 0 END) AS github2025,
+    SUM(CASE WHEN descSlugCurso = 'go-2026' THEN percentCursoCompleto ELSE 0 END) AS go2026,
+    SUM(CASE WHEN descSlugCurso = 'ia-canal-2025' THEN percentCursoCompleto ELSE 0 END) AS iaCanal2025,
+    SUM(CASE WHEN descSlugCurso = 'lago-mago-2024' THEN percentCursoCompleto ELSE 0 END) AS lagoMago2024,
+    SUM(CASE WHEN descSlugCurso = 'loyalty-predict-2025' THEN percentCursoCompleto ELSE 0 END) AS loyaltyPredict2025,
+    SUM(CASE WHEN descSlugCurso = 'machine-learning-2025' THEN percentCursoCompleto ELSE 0 END) AS machineLearning2025,
+    SUM(CASE WHEN descSlugCurso = 'matchmaking-trampar-de-casa-2024' THEN percentCursoCompleto ELSE 0 END) AS matchmakingTrampardecasa2024,
+    SUM(CASE WHEN descSlugCurso = 'ml-2024' THEN percentCursoCompleto ELSE 0 END) AS ml2024,
+    SUM(CASE WHEN descSlugCurso = 'mlflow-2025' THEN percentCursoCompleto ELSE 0 END) AS mlflow2025,
+    SUM(CASE WHEN descSlugCurso = 'nekt-2025' THEN percentCursoCompleto ELSE 0 END) AS nekt2025,
+    SUM(CASE WHEN descSlugCurso = 'pandas-2024' THEN percentCursoCompleto ELSE 0 END) AS pandas2024,
+    SUM(CASE WHEN descSlugCurso = 'pandas-2025' THEN percentCursoCompleto ELSE 0 END) AS pandas2025,
+    SUM(CASE WHEN descSlugCurso = 'plataforma-ml-2026' THEN percentCursoCompleto ELSE 0 END) AS plataformaMl2026,
+    SUM(CASE WHEN descSlugCurso = 'python-2024' THEN percentCursoCompleto ELSE 0 END) AS python2024,
+    SUM(CASE WHEN descSlugCurso = 'python-2025' THEN percentCursoCompleto ELSE 0 END) AS python2025,
+    SUM(CASE WHEN descSlugCurso = 'ragia' THEN percentCursoCompleto ELSE 0 END) AS ragia,
+    SUM(CASE WHEN descSlugCurso = 'speed-f1' THEN percentCursoCompleto ELSE 0 END) AS SpeedF1,
+    SUM(CASE WHEN descSlugCurso = 'sql-2020' THEN percentCursoCompleto ELSE 0 END) AS sql2020,
+    SUM(CASE WHEN descSlugCurso = 'sql-2025' THEN percentCursoCompleto ELSE 0 END) AS sql2025,
+    SUM(CASE WHEN descSlugCurso = 'streamlit-2025' THEN percentCursoCompleto ELSE 0 END) AS streamlit2025,
+    SUM(CASE WHEN descSlugCurso = 'trampar-lakehouse-2024' THEN percentCursoCompleto ELSE 0 END) AS tramparLakehouse2024,
+    SUM(CASE WHEN descSlugCurso = 'tse-analytics-2024' THEN percentCursoCompleto ELSE  0 END) AS tseAnalytics2024
 
 from tb_percent_curso
 
+GROUP BY idUsuario
